36440e1a0bc84f78df3a140e418496c452c880c7
Sophie Lambie <2764964l@student.gla.ac.uk>
2024-03-01 13:03:20 +0000

    base html file created
    basic html files created, views and urls set up, style sheet created but no content, no links

---
+++ bookle/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import url 
+from django.urls import include
+from bookle import views
+
+app_name = 'bookle'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+]
---
+++ bookle/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    context_dict = {}
+    return render(request, 'bookle/home.html', context = context_dict)
+
+def aboutus(request):
+    context_dict = {}
+    return render(request, 'bookle/aboutus.html', context = context_dict)
