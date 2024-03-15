e3bbd7b69644c0f02fffa0432794fc6316314dbc
phantomsheepp <rachel.horton1@icloud.com>
2024-03-15 00:25:53 +0000

    Added more admin views

--- bookle/admin.py
+++ bookle/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from bookle.models import UserProfile
+from bookle.models import UserProfile, Book, Puzzle, Score, Comment
 
-admin.site.register(UserProfile)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'release_year')
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+
+class PuzzleAdmin(admin.ModelAdmin):
+    list_display = ('puzzleID', 'date', 'difficulty', 'popularity')
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('scoreID', 'userID', 'guesses', 'puzzleID')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commentID', 'userID', 'puzzleID', 'comment')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Puzzle, PuzzleAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(Comment, CommentAdmin)
