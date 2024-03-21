e9c873cf4ce8cb1e8d5dd2f9947f521b40c04fe6
Sophie Lambie <2764964l@student.gla.ac.uk>
2024-03-21 13:28:08 +0000

    unit tests working

--- bookle/tests.py
+++ bookle/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from bookle.models import Book
+from django.urls import reverse
+from bookle.forms import RegisterForm, ProfileEditForm, UserForm
 
 # Create your tests here.
 
@@ -31,4 +33,44 @@ class BookModelTest(TestCase):
         book = Book.objects.get(id=1)
         country = book.country
         self.assertEqual(country, 'US')
+
+class TestRegisterForm(TestCase):
+   
+    def test_register_form_is_invalid(self):
+        form = RegisterForm(data={"email":"sophie@gmail..com", "bio":"This is my bio", "user_picture":""})
+        self.assertFalse(form.is_valid())
+
+class TestProfileEditForm(TestCase):
+
+    def test_profile_edit_form_is_valid(self):
+        form = ProfileEditForm(data={"bio": "Hi", "userpicture": "default.png" })
+        self.assertTrue(form.is_valid)
+
+class TestUserForm(TestCase):
+    
+    def test_user_form_is_valid(self):
+        form = UserForm(data={"password": "bookle123"})
+        self.assertTrue(form.is_valid)
+
+class HomeViewTest(TestCase):
+
+    def test_home_view(self):
+          response = self.client.get(reverse('bookle:home'))
+          self.assertEquals(response.status_code, 200)
+          self.assertTemplateUsed(response, 'bookle/home.html')
+
+class HomeViewTest(TestCase):
+
+    def test_home_view(self):
+          response = self.client.get(reverse('bookle:home'))
+          self.assertEquals(response.status_code, 200)
+          self.assertTemplateUsed(response, 'bookle/home.html')
+
+class LoginViewTest(TestCase):
+
+    def test_home_view(self):
+          response = self.client.get(reverse('bookle:login'))
+          self.assertEquals(response.status_code, 200)
+          self.assertTemplateUsed(response, 'bookle/login.html')
+
 
