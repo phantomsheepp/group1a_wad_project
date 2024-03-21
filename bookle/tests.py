3dae2bc40d469b74efd53b6289cc87923207b01f
Sophie Lambie <2764964l@student.gla.ac.uk>
2024-03-21 10:49:11 +0000

    one working unit test

--- bookle/tests.py
+++ bookle/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from bookle.models import Book
 
 # Create your tests here.
+
+class BookModelTest(TestCase):
+
+  class BookModelTest(TestCase):
+    def setUpTestData(self):
+        Book.objects.create(
+            isbn=9780141345635,
+            title='The Fault In Our Stars',
+            author='John Green',
+            genre='fiction',
+            release_year='2014',
+            country='US',
+            cover='',
+            description=''
+        )
+
+    def test_first_name_label(self):
+        book = Book.objects.get(id=1)
+        isbn = book.isbn
+        self.assertEqual(isbn, 9780141345635)
